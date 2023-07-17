30683c223ab200fb2b49754d15ffabc5a4f00bd1
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-16 22:14:07 -0400

    variables de entorno

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require("express");
 let app = express();
 
@@ -14,6 +15,10 @@ app.get("/", (req, res)  => {
 res.sendFile(pathUrl)
 })
 app.get("/json", (req, res) => {
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({ message: "HELLO JSON" });
+    } else {
     res.json({ message: "Hello json" });
+    }
 });
 module.exports = app;
