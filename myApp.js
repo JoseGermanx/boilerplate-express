d3146cde3ce0e0f21f680ec880e24a659066331e
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-16 21:49:12 -0400

    Sriviendo un path public

--- myApp.js
+++ myApp.js
@@ -2,10 +2,13 @@ let express = require("express");
 let app = express();
 
 const pathUrl = __dirname + '/views/index.html';
+const pathPublic = __dirname + '/public';
 
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
 // });
+
+app.use(express.static(pathPublic));
 
 app.get("/", (req, res)  => {
 res.sendFile(pathUrl)
